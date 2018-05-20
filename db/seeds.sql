8181bdcd0b7631bfceb8924dd3fcaf91816edd3f
Mark Hainline <wucheeman@gmail.com>
2018-05-20 08:50:28 -0400

    completed work

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgerzDB;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Bacon Cheddar Burger", true);
+
+INSERT INTO burgers(burger_name)
+VALUES ("Bacon Brie Mushroom Burger");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Bacon Fried Green Tomato Burger");

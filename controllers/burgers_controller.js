8d3df77b67874e63a60b553488efc93f455f32df
Mark Hainline <wucheeman@gmail.com>
2018-05-23 15:59:45 -0400

    initial pass at PUT function in burgers_controllers; supports single word names

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -21,6 +21,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create(
+    // TODO, does this need to be an array?
     [req.body.burger_name], 
     function(result) {
       // Send back the ID of the new burger
@@ -28,8 +29,17 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-
+router.put("/api/burgers/:name", function(req, res) {
+  const burger_name = req.params.burger_name;
+  console.log(burger_name);
+  burger.update (req.body.burger_name, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  })
 });
 
 // Export routes for server.js to use.

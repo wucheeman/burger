f3de857e91b46fe69f5a532132b65d2ad1ab5acc
Mark Hainline <wucheeman@gmail.com>
2018-05-24 10:56:59 -0400

    front end now enables entry of burger to be devoured; update of DB; and update of page showing new state

--- public/assets/js/front_end.js
+++ public/assets/js/front_end.js
@@ -1,24 +1,27 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  // code NOT TESTED!!!
-  // $(".devour").on("click", function(event) {
-  //   var burger_name = $(this).data("burger_name");
-
-  //   // Send the PUT request.
-  //   $.ajax("/api/burgers/devour" + id, {
-  //     type: "PUT",
-  //     data: burger_name
-  //   }).then(
-  //     function() {
-  //       console.log("devoured", newSleep);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     }
-  //   );
-  // });
+  
+  $(".devour-form").on("submit", function(event) {
+    event.preventDefault();
+    // var burger_name = $(this).data("burger_name");
+    var burgerToEat = {
+      burger_name: $("#bte").val().trim(),
+    };
+    console.log(burgerToEat);
+    // Send the PUT request.
+    $.ajax("/api/burgers/devour", {
+      type: "PUT",
+      data: burgerToEat
+    }).then(
+      function() {
+        console.log("devoured", burgerToEat);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
     var newBurger = {
       burger_name: $("#nb").val().trim(),

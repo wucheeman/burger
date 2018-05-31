f70f532d9569044cc7c67cafaa733747d164bcaa
Mark Hainline <wucheeman@gmail.com>
2018-05-31 16:49:46 -0400

    delinted front-end.js

--- public/assets/js/front_end.js
+++ public/assets/js/front_end.js
@@ -1,24 +1,13 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   
   $(".devour").on("click", function(event) {
-    // TODO delete?
-    // $(".devour").on("submit", function(event) {
     event.preventDefault();
-    console.log('got here');
     var burgerID = $(this).data("id");
-    // TODO: delete?
-    // var burgerToEat = {
-    //   burger_name: $("#bte").val().trim(),
-    // };
-    console.log(burgerID);
-    // Send the PUT request.
     $.ajax("/api/burgers/" + burgerID, {
       type: "PUT",
       data: burgerID
     }).then(
       function() {
-        console.log("devoured burger with ID: ", burgerID);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -30,7 +19,6 @@ $(function() {
     var newBurger = {
       burger_name: $("#nb").val().trim(),
     };
-    // console.log('length is ' + newBurger.burger_name.length);
     if (newBurger.burger_name.length > 25) {
       alert('Burger names must not be longer than 25 characters');
       return;
@@ -38,7 +26,6 @@ $(function() {
       alert('Burger names must have at least one character');
       return;
     }
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
@@ -50,6 +37,4 @@ $(function() {
       }
     );
   });
-
-
 });

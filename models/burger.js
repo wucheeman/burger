951258dc7b694c83a0d0e4bb7dbf92d4f0458475
Mark Hainline <wucheeman@gmail.com>
2018-05-31 16:46:46 -0400

    delinted

--- models/burger.js
+++ models/burger.js
@@ -9,7 +9,6 @@ const burger = {
     // returns all burgers in the database as an array of burger objects
     // TODO (future) convert to forEach()
     orm.selectAll(function(res) {
-      // console.log('in burger.all');
       const burgerArray = [];
       for (var i = 0; i < res.length; i++) {
         let daBurger = {};
@@ -18,15 +17,12 @@ const burger = {
         daBurger.devoured = res[i].devoured;
         burgerArray.push(daBurger);
       }
-      // console.log(burgerArray);
       cb(burgerArray);
     });
   },
   create: function(newBurger, cb) {
     // inserts new burger into DB with state = not devoured and returns true
-    // console.log('burger is creating a ' + newBurger);
     orm.insertOne(newBurger, false, function(res) {
-      // console.log(res);
       cb(res);
     });
   },
@@ -38,20 +34,5 @@ const burger = {
     });
   }
 };
-
-// Test code
-// burger.all(function (data) {
-//   console.log(data);
-// })
-
-// burger.create('REALLY BIG Plain Burger', function(data) {
-//   console.log(data);
-// });
-
-// burger.update("Bacon Brie Mushroom Burger", function(data) {
-//   console.log(data);
-// });
-
-
 
 module.exports = burger;
